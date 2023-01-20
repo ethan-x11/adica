f501bf50b5995e80fd943be2d185bfec99dd2cdd
ethan-x11 <subhamdutta96740@gmail.com>
2023-01-21 01:29:30 +0530

    basic structure

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Sidebar from '../components/Sidebar'
 
 const style ={
   wrapper: `flex justify-center h-screen select-none bg-[#15202b] text-white min-h-screen flex-col items-center justify-center py-2`,
@@ -17,7 +18,7 @@ const Home: NextPage = () => {
       
       <main className="flex w-full flex-1 flex-col items-center px-20 text-center">
           <div className={style.content}>
-            <h2>SideBar</h2>
+            <Sidebar/>
             <h2>Feed</h2>
             <h2>Widgets</h2>
           </div>
