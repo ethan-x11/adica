5938883fa4d4eff1c7c01a6ecbc4e5471a8ff173
ethan-x11 <subhamdutta96740@gmail.com>
2023-01-22 07:19:12 +0530

    smart contract

--- smart contract/hardhat.config.js
+++ smart contract/hardhat.config.js
@@ -3,4 +3,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/Mq9t3o9PwZXDVGP6JJZURUy8mQASJLBH",
+      accounts: ["dfe937276836c2315355e72c1a03f956ed525653cf2276f14421234072c425d9"],
+    }
+  }
 };
