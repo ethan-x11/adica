c14956dc8969d7408047e5a46461ea84f4cde007
ethan-x11 <subhamdutta96740@gmail.com>
2023-01-24 02:57:55 +0530

    Twitter App

--- client/client.js
+++
@@ -1,9 +0,0 @@
-import sanityClient from '@sanity/client'
-
-module.exports = sanityClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: 'production',
-  apiVersion: 'v1',
-  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
-  useCdn: false,
-})
